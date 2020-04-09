eee5e5c48d2819962ce4dabed923fff22993ceb3
Zedong Xun <31828034+rhsiun@users.noreply.github.com>
2020-04-09 13:57:31 -0500

    api

---
+++ app/src/main/java/com/example/cse438/cse438_assignment2/Data/TrendingResult.kt
@@ -0,0 +1,20 @@
+package com.example.cse438.cse438_assignment2.Data
+
+//Movie
+data class TrendingResult(
+    val id: Int,
+    val video: Boolean,
+    val vote_count: Int,
+    val vote_average: Double,
+    val title: String,
+    val release_date: String,
+    val original_language: String,
+    val original_title: String,
+    val genre_ids: Array<Int>,
+    val backdrop_path: String,
+    val adult: Boolean,
+    val overview: String,
+    val poster_path: String,
+    val popularity: Long,
+    val media_type: String
+)
--- app/src/main/java/com/example/cse438/cse438_assignment2/network/ApiClient.kt
+++ app/src/main/java/com/example/cse438/cse438_assignment2/network/ApiClient.kt
@@ -6,13 +6,13 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 //base retrofit client to define settings and url
 object ApiClient {
-    const val BASE_URL = "https://api.deezer.com/"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
 
-    fun makeRetrofitService(): TrackInterface {
+    fun makeRetrofitService(): MovieInterface {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
-            .build().create(TrackInterface::class.java)
+            .build().create(MovieInterface::class.java)
     }
 
 //    fun makeRetrofitServicewithParameter(url: String): TrackInterface {
