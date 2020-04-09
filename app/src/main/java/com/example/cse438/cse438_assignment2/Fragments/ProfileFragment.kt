3fa95be7ba3b324ba0738fb9d0ecd4e564d413dc
jennyxp <42778124+jennyxp@users.noreply.github.com>
2020-04-09 13:02:50 -0500

    profile layout

--- app/src/main/java/com/example/cse438/cse438_assignment2/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/cse438/cse438_assignment2/Fragments/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cse438.cse438_assignment2.Fragments
 
+import android.app.AlertDialog
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -7,12 +8,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cse438.cse438_assignment2.Adapter.PlayListAdapter
+import com.example.cse438.cse438_assignment2.Data.Track
+import com.example.cse438.cse438_assignment2.Database.PlayList
+import com.example.cse438.cse438_assignment2.PlayListViewModel
 
 import com.example.cse438.cse438_assignment2.R
+import kotlinx.android.synthetic.main.add_new_playlist.*
+import kotlinx.android.synthetic.main.add_new_playlist.view.*
+import kotlinx.android.synthetic.main.fragment_playlist.*
 
 
 class ProfileFragment : Fragment() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +40,13 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onStart() {
+        super.onStart()
+    }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-
+    }
 }
