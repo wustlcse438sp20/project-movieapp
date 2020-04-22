31f7a8d15f6fde14cb72b42244ea5d129c1a7413
Zedong Xun <31828034+rhsiun@users.noreply.github.com>
2020-04-22 18:39:44 -0500

    saorui

--- app/src/main/java/com/example/cse438/cse438_assignment2/ShowMovieActivity.kt
+++ app/src/main/java/com/example/cse438/cse438_assignment2/ShowMovieActivity.kt
@@ -103,7 +103,7 @@ class ShowMovieActivity : AppCompatActivity() {
                 youTubePlayer.cueVideo(videoId, 0f)
             }
         })
-        playListViewModel = ViewModelProvider(this,PlayListViewModelFactory2(this!!.application,"")).get(PlayListViewModel2::class.java)
+        playListViewModel = ViewModelProvider(this).get(PlayListViewModel2::class.java)
 
         //observe the allEvents LiveData
         playListViewModel!!.allPlaylists.observe(this, Observer { playlists ->
